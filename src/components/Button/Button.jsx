51e9419f427ab911c48a75d9a29948531a3c1d0b
Diego Parreira <diego.parreira94@gmail.com>
2020-11-05 11:38:57 -0300

    button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
 
-const Button = () => {
+const Button = (text) => {
     return (
-        <div>
-            
-        </div>
+        <Button variant="contained" color="primary">
+            {text}
+        </Button>
     )
 }
 
