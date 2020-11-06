4d44080f9f8e8b51df50d67114b587b690d39f93
Felipe Brito <brito.feh@gmail.com>
2020-11-06 20:39:00 -0300

    Styled search component and folder organization

--- src/core/theme/theme.js
+++ src/core/theme/theme.js
@@ -1,38 +1,67 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-        main: '#115D8C',
+    overrides: {
+        MuiFormControlLabel: {
+            label: {
+                color: "#F1F1F1",
+            },
+        },
+        MuiOutlinedInput: {
+            colorSecondary: {
+                backgroundColor: "#F1F1F1",
+                color: "#123952",
+            },
+        },
+        MuiFormLabel: {
+            colorSecondary: {
+                textAlign: "left",
+                color: "#F1F1F1",
+            },
+        },
+        MuiRadio: {
+            colorSecondary: {
+                color: "#F1F1F1",
+            },
+        },
+        MuiButton: {
+            contained: {
+                height: "57px",
+            },
+        },
     },
-    secondary: {
-        main: '#F25D27',
-        contrastText: '#F1F1F1',
+    palette: {
+        primary: {
+            main: "#115D8C",
+        },
+        secondary: {
+            main: "#F25D27",
+            contrastText: "#F1F1F1",
+        },
+        text: {
+            primary: "#123952",
+            secondary: "#617480",
+            hint: "#A0ACB2",
+        },
     },
-    text:{
-        primary:'#123952',
-        secondary: '#617480',
-        hint: '#A0ACB2',
-    }
-  },
-  typography:{
-      h1:{
-        fontFamily:'Barlow',
-        fontSize: '2rem'
-      },
-      h2:{
-        fontFamily: 'Barlow',
-        fontSize: '1.5rem'
-      },
-      body1:{
-        fontFamily: 'Heebo',
-        font: '1rem'
-      },
-      body2:{
-        fontFamily:'Heebo',
-        fontSize: '0.8rem'
-      },
-  }
+    typography: {
+        h1: {
+            fontFamily: "Barlow",
+            fontSize: "2rem",
+        },
+        h2: {
+            fontFamily: "Barlow",
+            fontSize: "1.5rem",
+        },
+        body1: {
+            fontFamily: "Heebo",
+            font: "1rem",
+        },
+        body2: {
+            fontFamily: "Heebo",
+            fontSize: "0.8rem",
+        },
+    },
 });
 
 export default theme;
--- src/molecules/SearchBar/SearchBar.jsx
+++ src/molecules/SearchBar/SearchBar.jsx
@@ -1,104 +1,34 @@
-import React from 'react'
-import {
-  Grid,
-  FormControl,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  FormGroup,
-  Box,
-} from '@material-ui/core'
-import LocationOnIcon from '@material-ui/icons/LocationOn'
-import GpsFixedIcon from '@material-ui/icons/GpsFixed'
-import PersonIcon from '@material-ui/icons/Person'
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday'
-import CustomInput from '../../components/CustomInput/CustomInput'
-import CustomButton from '../../components/CustomButton/CustomButton'
-import { makeStyles } from '@material-ui/core/styles'
-import theme from '../../core/theme/theme'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexWrap: 'nowrap',
-  },
-}))
+import React from "react";
+import Box from "@material-ui/core/Box";
+import theme from "../../core/theme/theme";
+import SearchBarFields from "./components/SearchBarFields";
 
 const SearchBar = () => {
-  const classes = useStyles()
-
   const onChangeOneWay = (e) => {
-    console.log(e)
-  }
+    console.log(e);
+  };
   const onChangeInput = (e) => {
-    console.log(e)
-  }
+    console.log(e);
+  };
 
   const sendSearchData = (e) => {
-    console.log(e)
-  }
+    console.log(e);
+  };
 
   return (
     <Box
       bgcolor={theme.palette.primary.main}
-      padding='20px 50px'
-      borderRadius='4px'
+      padding="18px 18px"
+      borderRadius="4px"
+      justifyContent="center"
     >
-      <Grid container direction='column'>
-        <FormControl>
-          <RadioGroup
-            row
-            defaultValue='oneWay'
-            onChange={onChangeOneWay.bind(this)}
-          >
-            <FormControlLabel
-              value='oneWay'
-              control={<Radio />}
-              label='Somente Ida'
-            />
-            <FormControlLabel
-              value='twoWay'
-              control={<Radio />}
-              label='Ida e Volta'
-            />
-          </RadioGroup>
-        </FormControl>
-        <Grid container direction='row' wrap='nowrap'>
-          <CustomInput
-            iconData={<GpsFixedIcon />}
-            type='input'
-            name='Origem'
-          ></CustomInput>
-          <CustomInput
-            iconData={<LocationOnIcon />}
-            type='input'
-            name='Destino'
-          ></CustomInput>
-          <FormGroup label='Datas' row className={classes.root}>
-            <CustomInput
-              iconData={<CalendarTodayIcon />}
-              type='date'
-              name=''
-            ></CustomInput>
-            <CustomInput
-              iconData={<CalendarTodayIcon />}
-              type='date'
-              name=''
-            ></CustomInput>
-          </FormGroup>
-          <CustomInput
-            iconData={<PersonIcon />}
-            type='select'
-            name=''
-          ></CustomInput>
-          <CustomButton
-            text='Enviar'
-            iconName='send'
-            onClick={sendSearchData.bind(this)}
-          ></CustomButton>
-        </Grid>
-      </Grid>
+      <SearchBarFields
+        sendSearchData={sendSearchData}
+        onChangeInput={onChangeInput}
+        onChangeOneWay={onChangeOneWay}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
