cea30d953857636c2030df9a2815f1c58bd0d4e4
Felipe Brito <brito.feh@gmail.com>
2020-11-08 19:14:32 -0300

    logo update to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,46 @@
 import "../Login/Login.css";
 
 import React from "react";
-import LogoFrag from "../../components/Logo/Logo";
+import Logo from "../../components/Logo/Logo";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import CustomInput from "../../components/CustomInput/CustomInput";
 
-function Login() {
-    return (
-        <div className="base-container">
-            <div className="content">
-                <div className="logo">
-                    <LogoFrag large/>
-                </div>
-                
-                <span className="logon">Faça seu Logon</span>
-
-                <div className="form">
-                    <form>
-                        <div className="form-group">
-                            <CustomInput name="E-mail" type="text"/>
-                        </div>
-                        
-                        <div className="form-group">
-                            <CustomInput name="Senha" type="password"/>
-                        </div>
-
-                        <div className="send-button">
-                            <CustomButton text="Entrar" iconName="send" color="secondary"/>
-                        </div>
-                    </form>
-                </div>
-
-                <a href="#" className="forgot"><span>Esqueci minha senha</span></a>
-
-                <a href="#" className="account"><span>Criar conta</span></a>
-            </div>
+const Login = () => {
+  return (
+    <div className="base-container">
+      <div className="content">
+        <div className="logo">
+          <Logo large />
         </div>
-    )
-}
 
-export default Login
+        <span className="logon">Faça seu Logon</span>
+
+        <div className="form">
+          <form>
+            <div className="form-group">
+              <CustomInput name="E-mail" type="text" />
+            </div>
+
+            <div className="form-group">
+              <CustomInput name="Senha" type="password" />
+            </div>
+
+            <div className="send-button">
+              <CustomButton text="Entrar" iconName="send" color="secondary" />
+            </div>
+          </form>
+        </div>
+
+        <a href="#" className="forgot">
+          <span>Esqueci minha senha</span>
+        </a>
+
+        <a href="#" className="account">
+          <span>Criar conta</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
