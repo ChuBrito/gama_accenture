c6456ce43bec3ff0925a34b35fc86759115dfc12
Diego Parreira <diego.parreira94@gmail.com>
2020-11-09 14:17:49 -0300

    resolve app

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,24 +1,12 @@
 import "./App.css";
 import theme from "../../core/theme/theme";
-import { Container, ThemeProvider } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
-import CustomButton from "../../components/CustomButton/CustomButton";
-import CustomInput from "../../components/CustomInput/CustomInput";
-// import CardList from "../../molecules/CardList/CardList";
-
+import { ThemeProvider } from "@material-ui/core";
+import CardList from "../../molecules/CardList/CardList";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Container className="App">
-        <Typography variant="h1">Projeto inicial React H1</Typography>
-        <Typography variant="h2">Projeto inicial React H2</Typography>
-        <Typography variant="body1">Projeto inicial React body1</Typography>
-        <Typography variant="body2">Projeto inicial React body2</Typography>
-      </Container>
-      <CustomButton text="Teste" iconName="send" color="secondary"/>
-      <CustomInput name="email" type="text" iconData="send"/>
-    {/* <CardList /> */}
+      <CardList />
     </ThemeProvider>
   );
 };
