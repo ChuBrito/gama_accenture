9c21b9c8ac52fb802d9660e6a714a5a8b46dc758
Felipe Brito <brito.feh@gmail.com>
2020-11-09 17:18:24 -0300

    adaptação do login, logo e deafaultpage

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,9 +4,9 @@ import { ReactComponent as LogoLarge } from "../../assets/Logo_lg.svg";
 import { ReactComponent as LogoSmall } from "../../assets/Logo_sm.svg";
 import { ReactComponent as LogoLine } from "../../assets/Logo_line.svg";
 
-const Logo = ({ large, line, height }) => {
+const Logo = ({ large, line, height, width }) => {
   return (
-    <Box display="flex" height={height}>
+    <Box display="flex" maxHeight={height} maxWidth={width}>
       {line ? <LogoLine /> : large ? <LogoLarge /> : <LogoSmall />}
     </Box>
   );
--- src/templates/DefaultPage/DefaultPage.jsx
+++ src/templates/DefaultPage/DefaultPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Container } from "@material-ui/core";
+import React from "react";
+import Header from "../../organisms/Header/Header";
 
-const DefaultPage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const DefaultPage = (props) => {
+  return (
+    <Container style={{ paddingTop: "96px", minHeight: "100vh" }}>
+      <Header />
+      {props.children}
+    </Container>
+  );
+};
 
 export default DefaultPage;
