e37de7366599092279fd92e4aec5bb90c9b265e0
Felipe Brito <brito.feh@gmail.com>
2020-11-11 19:55:43 -0300

    search mock list

--- src/organisms/SearchList/SearchList.jsx
+++ src/organisms/SearchList/SearchList.jsx
@@ -1,21 +1,44 @@
+import { Box, Divider, Grid, GridList, List } from "@material-ui/core";
 import React from "react";
+import InfoCard from "../../components/InfoCard/InfoCard";
 
 const MOCKITEM = {
-  initialDestination='',
-  finalDestination,
-  flyType,
-  value,
-  airline,
-  flyInfo,
-  data: {
-    initialDate,
-    arriveDate,
+  initialDestination: "New York",
+  finalDestination: "Madrid",
+  flyType: "oneWay",
+  value: 455.45,
+  airline: "Gol",
+  flyInfo: "",
+  date: {
+    initialDate: "12/04/2021",
+    arriveDate: "12/05/2021",
   },
 };
 
 const MOCKLIST = {};
-const SearchList = (searchList) => {
-  return <div></div>;
+const SearchList = (searchResult) => {
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box
+        id="filtroField"
+        display="flex"
+        flexDirection="row"
+        justifyContent="flexEnd"
+      >
+        <Box id="filtro"></Box>
+      </Box>
+      <List id="resultList">
+        <InfoCard data={MOCKITEM} />
+
+        <InfoCard data={MOCKITEM} />
+        <InfoCard data={MOCKITEM} />
+        <InfoCard data={MOCKITEM} />
+        <InfoCard data={MOCKITEM} />
+        <InfoCard data={MOCKITEM} />
+        <InfoCard data={MOCKITEM} />
+      </List>
+    </Box>
+  );
 };
 
 export default SearchList;
