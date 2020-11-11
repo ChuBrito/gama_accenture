5453cf1d39563f27f52d2545f3e8126a53d59fd0
Felipe Brito <brito.feh@gmail.com>
2020-11-11 19:55:26 -0300

    search mounting pages

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DefaultPage from "../../templates/DefaultPage/DefaultPage";
 import SearchBar from "../../organisms/SearchBar/SearchBar";
+import SearchList from "../../organisms/SearchList/SearchList";
 
 const Search = () => {
   return (
     <DefaultPage>
       <SearchBar></SearchBar>
+      <SearchList></SearchList>
     </DefaultPage>
   );
 };
