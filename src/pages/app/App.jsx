dede1510ada40510499e6edfdc8b1b550ac43ddd
Diego Parreira <diego.parreira94@gmail.com>
2020-11-11 11:27:15 -0300

    Correçao do package.json criacao das rotas

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import theme from "../../core/theme/theme";
-import {  ThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import Login from "../Login/Login";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Login/>
+      <Login />
     </ThemeProvider>
   );
 };
