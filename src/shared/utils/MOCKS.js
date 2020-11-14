ec58b64ab77735d72300cd3519fa14b60d796075
Luiz Fernando Thomaz <lft.thomaz@gmail.com>
2020-11-14 19:13:38 -0300

    adding favorite functionality

--- src/shared/utils/MOCKS.js
+++ src/shared/utils/MOCKS.js
@@ -4,6 +4,7 @@ export const TICKET_MOCK_1 = {
     finalDestination: "Madrid",
     finalIataCode: "MAD",
     oneWay: true,
+    favorited: false,
     value: 455.45,
     flyInfos: [{
             //infos da ida
@@ -38,6 +39,7 @@ export const TICKET_MOCK_2 = {
     finalIataCode: "BER",
     oneWay: false,
     value: 385.45,
+    favorited: false,
     flyInfos: [{
             //infos da ida
             airline: "Air France", // ele envia o codigo ai tem que traduzir pelo campo dictionaries no final da response
